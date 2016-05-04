c9a8bbdaec3c4f28462cd1401fecdfdb66237bdd
banjun <banjun@gmail.com>
2016-05-04 19:09:46 +0900

    move or copy by tapping arisu or not

--- ArisuInFact/ViewController.swift
+++ ArisuInFact/ViewController.swift
@@ -13,7 +13,22 @@ import Ikemen
 
 
 class ViewController: UIViewController {
-    private let imageView = UIImageView(frame: .zero) ※ {$0.contentMode = .ScaleAspectFit}
+    private let imageView: UIImageView = UIImageView(frame: .zero) ※ { v in
+        v.contentMode = .ScaleAspectFit
+    }
+    private lazy var yesButton: UIButton = UIButton(type: .System) ※ { b in
+        b.setTitle("ありす", forState: .Normal)
+        b.addTarget(self, action: #selector(yes), forControlEvents: .TouchUpInside)
+    }
+    private lazy var noButton: UIButton = UIButton(type: .System) ※ { b in
+        b.setTitle("ちがう", forState: .Normal)
+        b.addTarget(self, action: #selector(no), forControlEvents: .TouchUpInside)
+    }
+    
+    let sourceFolderPath = "/codefirst/Products/LoveLiver/face/_undetermined" // show images and user determine whether yes or no
+    let yesDestinationFolderPath = "/codefirst/Products/LoveLiver/face/arisu" // where move yes images
+    let noDestinationFolderPath = "/codefirst/Products/LoveLiver/face/non-arisu" // where move no images
+    var undeterminedFaces = [(name: String, image: UIImage)]()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -27,13 +42,22 @@ class ViewController: UIViewController {
     override func loadView() {
         super.loadView()
         
+        edgesForExtendedLayout = .None
         view.backgroundColor = .whiteColor()
         
         let autolayout = view.northLayoutFormat(["p": 8], [
             "image": imageView,
+            "yes": yesButton,
+            "no": noButton,
+            "spacerT": MinView(),
+            "spacerB": MinView(),
             ])
         autolayout("H:|[image]|")
-        autolayout("V:|[image]|")
+        autolayout("H:|-p-[no]-p-[yes(==no)]-p-|")
+        autolayout("V:|-p-[spacerT][image]")
+        autolayout("V:[image][yes][spacerB]")
+        autolayout("V:[image][no][spacerB]")
+        autolayout("V:[spacerB(==spacerT)]-p-|")
     }
     
     override func viewDidLoad() {
@@ -46,20 +70,31 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         
         if let client = Dropbox.authorizedClient {
-            client.files.listFolder(path: "/codefirst/Products/LoveLiver/face/tulip2").response { (result, error) in
+            client.files.listFolder(path: sourceFolderPath).response { (result, error) in
                 NSLog("%@", "result = \(result), error = \(error)")
+                
+                client.files.createFolder(path: self.yesDestinationFolderPath)
+                client.files.createFolder(path: self.noDestinationFolderPath)
+                
                 for entry in result?.entries ?? [] {
                     client.files.download(path: entry.pathLower, destination: { (url, response) -> NSURL in
                         let u = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent(entry.name)
                         let _ = try? NSFileManager.defaultManager().removeItemAtURL(u)
                         return u
                     }).response({ entryAndURL, error in
-                        guard let file = entryAndURL?.1.path else { return }
-                        self.imageView.image = UIImage(contentsOfFile: file)
+                        guard let file = entryAndURL?.1.path,
+                            let image = UIImage(contentsOfFile: file) else { return }
+                        
+                        self.undeterminedFaces.append((name: entry.name, image: image))
+                        self.reloadImageView()
                     })
                 }
             }
         }
+    }
+    
+    private func reloadImageView() {
+        imageView.image = undeterminedFaces.first?.image
     }
     
     @objc private func signin() {
@@ -68,5 +103,39 @@ class ViewController: UIViewController {
         }
         Dropbox.authorizeFromController(self)
     }
+    
+    @objc private func yes() {
+        determineToDestination(yesDestinationFolderPath)
+    }
+    
+    @objc private func no() {
+        determineToDestination(noDestinationFolderPath, copy: true)
+    }
+    
+    private func determineToDestination(destinationPath: String, copy: Bool = false) {
+        guard let face = undeterminedFaces.first,
+            let client = Dropbox.authorizedClient else { return }
+        
+        let from = sourceFolderPath + "/" + face.name
+        let to = destinationPath + "/"  + face.name
+        if copy {
+            client.files.copy(fromPath: from, toPath: to).response { (metadata, error) in
+                NSLog("%@", "files.copy metadata: \(metadata) error: \(error)")
+            }
+        } else {
+            client.files.move(fromPath: from, toPath: to).response { (metadata, error) in
+                NSLog("%@", "files.move metadata: \(metadata) error: \(error)")
+            }
+        }
+        
+        undeterminedFaces.removeFirst()
+        reloadImageView()
+    }
 }
 
+
+class MinView: UIView {
+    override func intrinsicContentSize() -> CGSize {
+        return CGSizeZero
+    }
+}
